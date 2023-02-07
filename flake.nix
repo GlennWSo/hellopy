747357f5aa3ff8a63bbf3fe7495079d1f2d52dc7
GlennWSo <sondellglenn@gmail.com>
2023-02-07 09:15:27 +0100

    hello

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.python39Packages.buildPythonPackage{ 
+          pname = "pyhello";
+          version = "0.13.37";
+          src = ./hello;  
+        };
+      });
+
+    };
+}
---
+++ hello/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="hello",
+    version="0.13.37",
+    description="derp",
+    author="It me!",
+    author_email="gward@python.net",
+    py_modules=["hello"],
+)
